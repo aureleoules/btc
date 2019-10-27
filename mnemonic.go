02115db485797a39405698cec2ac273effd40b40
unknown <aurele@oules.com>
2019-10-27 21:42:04 +0100

    mnemonic to hash

--- mnemonic.go
+++ mnemonic.go
@@ -3,11 +3,75 @@ package btc
 import (
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"log"
 	"math/big"
 	"strings"
 )
+
+// NewSeedFromMnemonic returns the hash of the mnemonic
+func NewSeedFromMnemonic(mnemonic string, passphrase string, wordlist Wordlist) ([]byte, error) {
+	words := strings.Split(mnemonic, " ")
+	binary := ""
+	for _, word := range words {
+		index := -1
+		for i := range wordlist {
+			if word == wordlist[i] {
+				index = i
+				break
+			}
+		}
+		if index < 0 {
+			return nil, errors.New("invalid mnemonic")
+		}
+
+		indexBinary := fmt.Sprintf("%b", index)
+		for len(indexBinary) < 11 {
+			indexBinary = "0" + indexBinary
+		}
+
+		binary += indexBinary
+	}
+
+	hash := ""
+	for i := 0; i < len(binary); i += 4 {
+		n := ""
+		for j := i; j-i < 4; j++ {
+			n += string(binary[j])
+		}
+		log.Println(n)
+		nInt, _ := new(big.Int).SetString(n, 2)
+		hexa := bigIntToHex(nInt)
+		hash += hexa
+	}
+
+	checksum := hash[len(hash)-2 : len(hash)]
+
+	sha := sha256.New()
+	decoded, err := hex.DecodeString(hash[0 : len(hash)-2])
+	if err != nil {
+		return nil, err
+	}
+	sha.Write(decoded)
+	shaHash := sha.Sum(nil)
+
+	shaHashBig := new(big.Int).SetBytes(shaHash)
+	shaHashHex := bigIntToHex(shaHashBig)
+
+	for len(shaHashHex) < 64 {
+		shaHashHex = "0" + shaHashHex
+	}
+
+	if shaHashHex[0:2] != checksum {
+		return nil, errors.New("invalid checksum")
+	}
+
+	log.Println(shaHashHex, len(shaHashHex))
+	log.Println(hash, checksum)
+	return nil, nil
+}
 
 // NewMnemonic generates a new mnemonic phrase
 func NewMnemonic(words int, wordlist Wordlist) (string, error) {
--- mnemonic_test.go
+++ mnemonic_test.go
@@ -7,78 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func TestNewMnemonic(t *testing.T) {
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, FrenchWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, EnglishWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, JapaneseWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, KoreanWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, SpanishWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, ItalianWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, ChineseSimplifiedWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, ChineseTraditionalWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	for i := 0; i < 5; i++ {
-// 		phrase, err := NewMnemonic(24, CzechWordlist)
-// 		assert.Nil(t, err)
-// 		phraseArray := strings.Split(phrase, " ")
-// 		assert.Equal(t, 24, len(phraseArray))
-// 	}
-
-// 	_, err := NewMnemonic(24, nil)
-// 	assert.NotNil(t, err)
-
-// 	_, err = NewMnemonic(-5, FrenchWordlist)
-// 	assert.NotNil(t, err)
-// }
-
 func TestNewMnemonic(t *testing.T) {
 
 	var wordLength = []int{
@@ -86,11 +14,16 @@ func TestNewMnemonic(t *testing.T) {
 	}
 
 	for _, l := range wordLength {
-		for i := 0; i < 100000; i++ {
+		for i := 0; i < 10000; i++ {
 			phrase, err := NewMnemonic(l, EnglishWordlist)
 			assert.Nil(t, err)
 			assert.Equal(t, l, len(strings.Split(phrase, " ")))
 		}
 	}
+}
+
+func TestNewSeedFromMnemonic(t *testing.T) {
+
+	NewSeedFromMnemonic("march assault engine warrior talent swarm pluck job prepare knife pipe man student dice receive analyst salute art clean wood enemy tourist lunch like", "", EnglishWordlist)
 
 }
